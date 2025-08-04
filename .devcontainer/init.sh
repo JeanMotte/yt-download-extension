f9d59f1be953a0b1212bfb90ed3aa59aa168c2dc
Jean Motte <jean.motte@polynom.io>
2025-08-04 16:11:14 +0000

    INIT: devcontainer

---
+++ .devcontainer/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e # Exit immediately if a command fails
+
+echo "--- ✅ Dev container is ready ---"
+echo "--- 🚀 Run 'pnpm create wxt@latest' to start a new project, or 'pnpm install' if you already have one. ---"
