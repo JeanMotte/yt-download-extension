fa231fcf466438174c2ecf4bf2db137631b51ed4
Jean Motte <jean.motte@polynom.io>
2025-08-05 10:31:01 +0000

    ADD(api): api files

---
+++ src/api/models/index.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AudioOption';
+export * from './DownloadRequest';
+export * from './DownloadSampleRequest';
+export * from './FormatsResponse';
+export * from './HTTPValidationError';
+export * from './History';
+export * from './ResolutionOption';
+export * from './TokenResponse';
+export * from './UserRead';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
+export * from './VideoURL';
