ee364a9c32a48c244172a484f99cb4e0b0580169
Jean Motte <jean.motte@polynom.io>
2025-08-11 11:02:23 +0000

    UPDATE

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,9 +8,9 @@ interface LayoutProps {
 export const Layout: React.FC<LayoutProps> = ({ children, onLogout }) => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px', borderBottom: '1px solid #ccc' }}>
-        <h1>YouLoad</h1>
-        <button onClick={onLogout}>Logout</button>
+      <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingInline: '10px' }}>
+        <p><strong>YouLoad</strong></p>
+        <button onClick={onLogout} style={{ padding: '1px'}}><i className="ti ti-power" /></button>
       </header>
       <main style={{ flex: 1, padding: '10px' }}>
         {children}
