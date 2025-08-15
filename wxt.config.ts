16f225ca30592759454d1bec5f7c376207189783
Jean Motte <jean.motte@polynom.io>
2025-08-15 19:03:57 +0000

    UPDATE: manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,15 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: () => ({
-    name: 'YouLoad',
+    name: 'YouLoad: Video Downloader',
+    description: 'Download YouTube Shorts and sample videos.',
+    version: '1.0.0',
+    icons: {
+      '16': 'icon-16.png',
+      '32': 'icon-32.png',
+      '48': 'icon-48.png',
+      '128': 'icon-128.png',
+    },
     permissions: ['identity', 'tabs', 'storage', 'downloads', 'sidePanel', 'cookies'],
     oauth2: {
       client_id: import.meta.env.WXT_GOOGLE_CLIENT_ID || 'YOUR_GOOGLE_OAUTH_CLIENT_ID',
@@ -19,9 +27,8 @@ export default defineConfig({
       default_title: 'YouLoad',
     },
     host_permissions: [
-      "http://localhost:8000/*",
       "https://youload-service.onrender.com/*",
       "*://*.youtube.com/",
-    ]
+    ],
   }),
 });
